a13e496fe50c215f342a04c4354b9c8d7c32dd8d
LENHASS <samuelmoraissm.02@gmail.com>
2026-05-25 23:20:06 -0300

    Add files via upload

---
+++ ESTRUTURA DE DADOS/EXEMPLO DE CLASS JAVA/Calculo.java
@@ -0,0 +1,7 @@
+public class Calculo{
+	public static double pi = 3.14;
+
+	public static double area(double r){
+		return pi*r*r;
+	}
+}
---
+++ ESTRUTURA DE DADOS/EXEMPLO DE CLASS JAVA/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+	public static void main(String[] args){
+			System.out.println(Calculo.area(10.0));
+	}
+
+}
